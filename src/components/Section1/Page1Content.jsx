a25d0e3e85552b09d2cf764811eaf9298bc9bde8
prashanth <kasamsaimaniprashanth@gmail.com>
2025-12-05 23:55:37 +0530

    added the center section

--- src/components/Section1/Page1Content.jsx
+++ src/components/Section1/Page1Content.jsx
@@ -1,9 +1,11 @@
 import LeftContent from './LeftContent'
 import RightContent from './RightContent'
+import Center from './Center'
 const Page1Content = () => {
   return (
     <div className='py-3 px-18'>
         <LeftContent/>
+        <Center/>
         <RightContent/>
     </div>
   )
